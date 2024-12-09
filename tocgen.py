2010637293678e4fc4732c32e58289d97abced41
Bartosz Wiśniewski <bartek990@gmail.com>
2024-12-09 17:40:52 +0100

    Added dataclass Chapter, Refactored make_table_of_contents()

--- tocgen.py
+++ tocgen.py
@@ -1,7 +1,33 @@
 import sys
+from dataclasses import dataclass, field
 
 TOC_LINK = "↑[Table of Contents](#table-of-contents)↑"  # Link do spisu treści
 TOC_TITLE = "## Table of Contents"                      # Nagłówek spisu treści
+
+@dataclass(order=True, frozen=True)
+class Chapter:
+    line: str = field(repr=False)
+    tag: str = field(init=False)
+    number: str = field(init=False)
+    title: str = field(init=False)
+
+    def __post_init__(self):
+        rest, title = self.line.split('. ', 1)
+        tag, number = rest.split(' ')
+
+        object.__setattr__(self, 'tag', tag)
+        object.__setattr__(self, 'number', number)
+        object.__setattr__(self, 'title', title.replace('\n', ''))
+
+    def get_link(self) -> str:
+        """
+        Returns a link to header - a chapter line
+        :return:
+        """
+        # todo: find better solution for replacing special chars with '-'
+        return (f'\n{(self.tag.count('#') - 1) * "\t"}{self.number}. '
+                f'[{self.title}](#{self.number}-{self.title.lower().replace(' ', '-')})\n'.replace(':', ''))
+
 
 def clean_toc_links(lines: list[str]) -> list[str]:
     """
@@ -10,6 +36,7 @@ def clean_toc_links(lines: list[str]) -> list[str]:
     :return:
     """
     return [line if TOC_LINK not in line else '' for line in lines]
+
 
 def insert_toc_links(lines: list[str]) -> list[str]:
     """
@@ -21,6 +48,7 @@ def insert_toc_links(lines: list[str]) -> list[str]:
     lines.append(TOC_LINK)
     return lines
 
+
 def make_list_of_chapters(lines: list[str]) -> list[str]:
     """
     Returns a list of chapters
@@ -28,6 +56,7 @@ def make_list_of_chapters(lines: list[str]) -> list[str]:
     :return:
     """
     return [line for line in lines if line.startswith('# ') or line.startswith('## ')]
+
 
 def make_table_of_contents(chapters: list[str]):
     """
@@ -39,24 +68,12 @@ def make_table_of_contents(chapters: list[str]):
 
     # Make table of contents
     for chapter in chapters:
-        chapter = chapter.replace('\n', '')
-        chapter = chapter.replace('# ', '')
-        chapter = chapter.replace('#', '\t')
-
-        link = chapter.lower()
-        link = link.replace('.', '')
-        link = link.replace('\t', '')
-        link = link.replace(' ', '-')
-        link = '#' + link
-
-        chapter = chapter.replace('. ', '. [')
-        chapter = chapter + f"]({link})\n"
-        if not chapter.startswith('\t'):
-            chapter = chapter + '\n'
-        table_of_contents.append(chapter)
+        chapter = Chapter(chapter)
+        table_of_contents.append(chapter.get_link())
 
     table_of_contents.append('\n')
     return table_of_contents
+
 
 def parse_toc(source_path: str, output_path: str) -> None:
     """
@@ -81,6 +98,7 @@ def parse_toc(source_path: str, output_path: str) -> None:
     with open(output_path, 'w') as f:
         f.writelines(new_file)
 
+
 def main():
     match sys.argv[1]:
         case 'parse':
@@ -94,8 +112,15 @@ def main():
         case '--help':
             print('Aby dodać spis treści do pliku MarkDown użyj:\ntocgen.py parse plik_źródłowy plik_wynikowy')
 
+        case '--test':
+            chapter = Chapter('## 046. Defining Enums\n')
+
+            print(chapter)
+            print(chapter.get_link())
+
         case _:
             print(f'Brak komendy "{sys.argv[1]}".\nAby uzyskać pomoc wpisz "tocgen.py --help"')
+
 
 if __name__ == '__main__':
     main()
